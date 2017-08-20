909ff1f08b3d5150879d779a4f67b6523e45ce2d
Eoin Houlihan <ehoulih@tcd.ie>
2017-08-20 17:21:51 +0100

    Add ability to add and remove steps

--- src/Component/Track.purs
+++ src/Component/Track.purs
@@ -1,19 +1,19 @@
 module Component.Track where
 
 import Audio.Howler (HOWLER)
-import Control.Applicative (pure, when)
+import Control.Applicative (pure)
 import Control.Bind (bind, discard)
 import Control.Monad.Aff (Aff)
-import Data.Array ((!!))
-import Data.Eq (class Eq)
+import Data.Array ((!!), (..))
 import Data.Function (const, ($), (<<<))
 import Data.Functor ((<$>))
 import Data.HeytingAlgebra (not)
-import Data.Lens (Lens, use, (%=), (.=))
+import Data.Lens (Lens, use, (%=), (+=), (.=))
 import Data.Lens.Record (prop)
 import Data.Maybe (Maybe(..))
 import Data.NaturalTransformation (type (~>))
-import Data.Ord (class Ord)
+import Data.Ord ((<=))
+import Data.Ring ((-))
 import Data.Show (show)
 import Data.Symbol (SProxy(..))
 import Data.Unit (Unit, unit)
@@ -26,7 +26,7 @@ import Halogen.HTML.Properties as HP
 import Component.Step as Step
 import Sound (Sound(..), allSounds, playSound)
 
-type State = { sound :: Sound, playing :: Boolean }
+type State = { sound :: Sound, playing :: Boolean, steps :: Int }
 
 sound :: forall a b r. Lens { sound :: a | r } { sound :: b | r } a b
 sound = prop (SProxy :: SProxy "sound")
@@ -34,15 +34,18 @@ sound = prop (SProxy :: SProxy "sound")
 playing :: forall a b r. Lens { playing :: a | r } { playing :: b | r } a b
 playing = prop (SProxy :: SProxy "playing")
 
+steps :: forall a b r. Lens { steps :: a | r } { steps :: b | r } a b
+steps = prop (SProxy :: SProxy "steps")
+
 data Query a = PlayPause a
              | ChangeSound Int a
+             | AddStep a
+             | RemoveStep a
 
 type Input = Unit
 type Message = Void
 
-data Slot = StepSlot
-derive instance eqSlot :: Eq Slot
-derive instance ordSlot :: Ord Slot
+type Slot = Int
 
 track :: forall e. H.Component HH.HTML Query Input Message (Aff (howler :: HOWLER | e))
 track =
@@ -55,7 +58,7 @@ track =
   where
 
   initialState :: State
-  initialState = { sound: Cowbell, playing: false }
+  initialState = { sound: Cowbell, playing: false, steps: 1 }
 
   render :: State -> H.ParentHTML Query Step.Query Slot (Aff (howler :: HOWLER | e))
   render state =
@@ -72,22 +75,36 @@ track =
               then "Pause"
               else "Play"
           ]
-      , HH.slot StepSlot Step.step unit absurd
+      , HH.button
+          [ HE.onClick (HE.input_ AddStep) ]
+          [ HH.text "+" ]
+      , HH.button
+          [ HE.onClick (HE.input_ RemoveStep) ]
+          [ HH.text "-" ]
+      , HH.span_ (renderStep <$> 1..state.steps)
       ]
+
+  renderStep n = HH.slot n Step.step unit absurd
 
   eval :: Query ~> H.ParentDSL State Query Step.Query Slot Message (Aff (howler :: HOWLER | e))
   eval = case _ of
     PlayPause next -> do
       playing %= not
-      maybeStepIsOn <- H.query StepSlot $ H.request Step.IsOn
-      case maybeStepIsOn of
-        Nothing -> pure unit
-        Just stepIsOn -> when stepIsOn do
-          sound' <- use sound
-          H.liftEff $ playSound sound' 1.0
+      -- maybeStepIsOn <- H.query StepSlot $ H.request Step.IsOn
+      -- case maybeStepIsOn of
+      --   Nothing -> pure unit
+      --   Just stepIsOn -> when stepIsOn do
+      sound' <- use sound
+      H.liftEff $ playSound sound' 1.0
       pure next
     ChangeSound index next -> do
       case allSounds !! index of
         Nothing -> pure unit
         Just newSound -> sound .= newSound
       pure next
+    AddStep next -> do
+     steps += 1
+     pure next
+    RemoveStep next -> do
+     steps %= \n -> if n <= 1 then 1 else n - 1
+     pure next
